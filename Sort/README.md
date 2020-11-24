a4fc46c1e0e3d33a06ed1c091dffb59a0a454727
= <=>
2020-11-25 00:57:18 +0700

    Update

--- Sort/README.md
+++ Sort/README.md
@@ -3,10 +3,20 @@
 - Xét lần lượt các cặp 2 phần tử liên tiếp. Nếu phần tử đứng sau nhỏ hơn phần tử đứng trước, ta đổi chỗ 2 phần tử. Nói cách khác, phần tử nhỏ nhất sẽ nổi lên trên.
 - Lặp lại đến khi không còn 2 phần tử nào thỏa mãn. Có thể chứng minh được số lần lặp không quá N−1, do một phần tử chỉ có thể nổi lên trên không quá N−1 lần.
 - Độ phức tạp O(N*N)
+- Mô tả:
+<p align="center">
+  <img src="img/Bubble-sort-example.gif"/>
+</p>
+
 ## Sắp xếp chèn (Insertion Sort)
 - Ý tưởng chính của thuật toán là ta sẽ sắp xếp lần lượt từng đoạn gồm 1 phần tử đầu tiên, 2 phần tử đầu tiên, …, N phần tử.
 - Giả sử ta đã sắp xếp xong i phần tử của mảng. Để sắp xếp i+1 phần tử đầu tiên, ta tìm vị trí phù hợp của phần tử thứ i+1 và "chèn" nó vào đó.
 - Độ phức tạp O(N*N)
+- Mô tả:
+<p align="center">
+  <img src="img/Insertion-sort-example.gif"/>
+</p>
+
 ## Sắp xếp trộn (Merge sort)
 - Chia dữ liệu thành 2 phần và sắp xếp từng phần
 - Sau đó gộp 2 phần lại với nhau. Để gộp 2 phần, ta làm như sau:
@@ -14,10 +24,23 @@
     + So sánh 2 phần tử đầu tiên của 2 phần. Phần tử nhỏ hơn ta cho vào A và xóa khỏi phần tương ứng.
     + Tiếp tục như vậy đến khi ta cho hết các phần tử vào dãy A.
 - Độ phức tạp O(N*log(N))
+- Mô tả:
+<p align="center">
+  <img src="img/Merge_sort_animation2.gif"/>
+</p>
+<p align="center">
+  <img src="img/merge_sort.png"/>
+</p>
+
 ## Sắp xếp vun đống (HeapSort)
 - Ta lưu mảng vào CTDL Heap.
 - Ở mỗi bước, ta lấy ra phần tử nhỏ nhất trong heap, cho vào mảng đã sắp xếp.
 - Độ phức tạp O(N*log(N))
+- Mô tả:
+<p align="center">
+  <img src="img/Heapsort-example.gif"/>
+</p>
+
 ## Sắp xếp nhanh (QuickSort)
 - Chia dãy thành 2 phần, một phần "lớn" và một phần "nhỏ".
     + Chọn một khóa pivot
@@ -25,9 +48,19 @@
     +Những phần tử nhỏ hơn hoặc bằng pivot chia vào phần nhỏ.
 - Gọi đệ quy để sắp xếp 2 phần.
 - Độ phức tạp có thể là O(N.N) hoặc O(N.log(N) nếu chia 2 phần không tốt
+<p align="center">
+  <img src="img/Quicksort-example.gif"/>
+</p>
+<p align="center">
+  <img src="img/Sorting_quicksort_anim.gif"/>
+</p>
+
 ## Sắp xếp cơ số (RadixSort)
 - Khác với tất cả các thuật toán nêu trên, RadixSort không sử dụng việc so sánh 2 phần tử.
     + Đầu tiên, thuật toán sẽ chia các phần tử thành các nhóm, dựa trên chữ số cuối cùng (hoặc dựa theo bit cuối cùng, hoặc vài bit cuối cùng).
     +Sau đó ta đưa các nhóm lại với nhau, và được danh sách sắp xếp theo chữ số cuối của các phần tử. Quá trình này lặp đi lặp lại với chữ số át cuối cho tới khi tất cả vị trí chữ số đã sắp xếp.
 - Độ phức tạp O(N*log(max(a_i)))
 - Cái này không thể sắp xếp số thực
+<p align="center">
+  <img src="img/radixSort.gif"/>
+</p>
--- Sort/sort.py
+++ Sort/sort.py
@@ -1,3 +1,4 @@
+arr = [64, 34, 25, 12, 22, 11, 9]
 #Bubble sort
 def Bubble_sort(arr):
     time = 0
@@ -68,7 +69,115 @@ def Merge_Sort(arr):
             j += 1
             k += 1
     return arr,Time
-arr = [64, 34, 25, 12, 22, 11, 9] 
+ 
+#Heap_Sort 
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1     # left = 2*i + 1
+    r = 2 * i + 2     # right = 2*i + 2
+ 
+    # See if left child of root exists and is
+    # greater than root
+    if l < n and arr[largest] < arr[l]:
+        largest = l
+ 
+    # See if right child of root exists and is
+    # greater than root
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+ 
+    # Change root, if needed
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+        # Heapify the root.
+        heapify(arr, n, largest)
+# The main function to sort an array of given size
+def Heap_Sort(arr):
+    n = len(arr)
+    # Build a maxheap.
+    for i in range(n//2 - 1, -1, -1):
+        heapify(arr, n, i)
+ 
+    # One by one extract elements
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # swap
+        heapify(arr, i, 0)
+    return arr
+
+
+# Qick_sort
+def partition(arr, low, high):
+    i = (low-1)         # index of smaller element
+    pivot = arr[high]     # pivot
+    for j in range(low, high):
+        # If current element is smaller than or
+        # equal to pivot
+        if arr[j] <= pivot:
+            # increment index of smaller element
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low  --> Starting index,
+# high  --> Ending index
+# Function to do Quick sort
+def Quick_Sort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        pi = partition(arr, low, high)
+        # Separately sort elements before
+        # partition and after partition
+        Quick_Sort(arr, low, pi-1)
+        Quick_Sort(arr, pi+1, high)
+    return arr
+
+#Radix_Sort
+def countingSort(arr, exp1): 
+    n = len(arr) 
+    # The output array elements that will have sorted arr 
+    output = [0] * (n) 
+    # initialize count array as 0 
+    count = [0] * (10) 
+    # Store count of occurrences in count[] 
+    for i in range(0, n): 
+        index = (arr[i] / exp1) 
+        count[int(index % 10)] += 1
+    # Change count[i] so that count[i] now contains actual 
+    # position of this digit in output array 
+    for i in range(1, 10): 
+        count[i] += count[i - 1] 
+    # Build the output array 
+    i = n - 1
+    while i >= 0: 
+        index = (arr[i] / exp1) 
+        output[count[int(index % 10)] - 1] = arr[i] 
+        count[int(index % 10)] -= 1
+        i -= 1
+    # Copying the output array to arr[], 
+    # so that arr now contains sorted numbers 
+    i = 0
+    for i in range(0, len(arr)): 
+        arr[i] = output[i] 
+# Method to do Radix Sort 
+def Radix_Sort(arr): 
+    # Find the maximum number to know number of digits 
+    max1 = max(arr) 
+    # Do counting sort for every digit. Note that instead 
+    # of passing digit number, exp is passed. exp is 10^i 
+    # where i is current digit number 
+    exp = 1
+    while max1 / exp > 0: 
+        countingSort(arr, exp) 
+        exp *= 10
+    return arr
+
+print(Quick_Sort(arr,0,len(arr)-1))
+print(Radix_Sort(arr))
 '''(a,t1)=Bubble_sort(arr)
 print ('1. Giải Thuật Bubble_sort:')
 print(a)
@@ -79,9 +188,9 @@ print ('2. Giải Thuật Insertion_sort:')
 print(a)
 print("Tốc độ:") 
 print(t1)
-print(a)'''
+print(a)
 (a,t1)=Merge_Sort(arr)
 print ('3. Giải Thuật Merge_sort:')
 print(a)
 print("Tốc độ:") 
-print(t1)
+print(t1)'''
